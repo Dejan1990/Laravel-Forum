a54cd356dcfcc5d79f3f6269d85017f43525b9a0
Dejan1990 <dejan.simo@hotmail.com>
2024-01-10 17:03:45 +0100

    Add Post model, controller, factory and migration

---
+++ database/migrations/2024_01_10_154517_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();//constrained -> make sure that our database self manages when user is deleted; restrictOnDelete -> this cause a database exception if we attempt to delete a user who has posts
+            $table->string('title');
+            $table->longText('body'); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
